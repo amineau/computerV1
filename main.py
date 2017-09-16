78c44bb35ad41b23cf6240de9edca10318bbf60a
amineau <amineau@student.42.fr>
2017-09-16 09:11:01 +0200

    recup terms ok

--- main.py
+++ main.py
@@ -12,22 +12,33 @@ def error(str):
     print(str)
     sys.exit(-1)
 
+def merge_terms(terms):
+    result = [0] * 3
+    for item in range(3):
+        result[item] = terms[0][item] - terms[1][item]
+    return result
+
 def main():
     if len(sys.argv) <> 2:
         error('bar number of arguments')
     equation = sys.argv[1]
     terms = list()
     for part in equation.replace(' ', '').split('='):
-        term = list(filter(None, re.split('\+?(-?[0-9*\^xX]+)', part)))
-        for x in term:
-            match = re.search('[Xx](\^[0-9]+)?', x)
+        term = [0] * 3
+        # re.match('([0-9]+(\.[0-9]+)?\*)([xX](\^[0-9]+))+') // regex à finir
+        for x in list(filter(None, re.split('\+?(-?[0-9*\^xX]+)', part))):
+            match = re.search('(-?[0-9\.]+)?\*?[Xx]\^?([0-9]+)?', x)
             if match:
-                help(match)
-                print(match.group(0))
+                index = int(match.groups(1)[1])
+                if index not in range(3):
+                    error(index + ' is not a valid polynomial degree')
+                term[index] = float(term[index]) + float(match.groups(1)[0])
+            else:
+                print("0, ya pas")
         terms.append(term)
 
-    # print(terms)
-    # print(equation)
+
+    print(merge_terms(terms))
 
 if __name__ == '__main__':
         main()
