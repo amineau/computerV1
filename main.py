b06a5b28a2b0f5249781c91ab42e4f67621c09e5
Antoine MINEAU <amineau@student.42.fr>
2017-09-17 17:35:17 +0200

    tests ok

--- main.py
+++ main.py
@@ -10,22 +10,21 @@
 import sys
 import re
 
-def error(str):
+def print_error(str):
     print(str)
     sys.exit(-1)
 
 def print_reduce_form(monomials):
     tab = []
-    print(monomials)
     for index, elem in reversed(list(enumerate(monomials))):
         if elem:
             sign = '-' if elem < 0 else '+'
             if len(tab) or sign == '-':
                 tab.append(sign)
-            if abs(elem) <> 1:
+            if abs(elem) <> 1 or index == 0:
                 tab.append(str(convert_if_integer(abs(elem))))
                 if index:
-                    tab.append('*') 
+                    tab.append('*')
             if index:
                 x = 'X'
                 x += '^%d'%(index) if index > 1 else ''
@@ -33,7 +32,8 @@ def print_reduce_form(monomials):
     print("Reduced form: " + ' '.join(tab) + " = 0")
 
 def print_polynomial_degree(degree):
-    print("Polynomial degree: %d"%(degree))
+    if degree >= 0:
+        print("Polynomial degree: %d"%(degree))
 
 def print_discriminant(discriminant):
     print("Discriminant: %d"%(discriminant))
@@ -74,7 +74,7 @@ def merge_monomials(monomials):
     return result
     
 def discriminant(monomials):
-    # b^2 - 4 a c
+    # d = b^2 - 4 a c
     a = monomials[2]
     b = monomials[1]
     c = monomials[0]
@@ -97,24 +97,36 @@ def convert_if_integer(number):
     return int(number) if number.is_integer() else number
 
 def first_degree_solution(monomials):
+    # x = -b / a
     a = monomials[1]
     b = monomials[0]
     x = fraction(b*-1, a)
-    return x
+    return (x,)
 
 def second_degree_solution(monomials, discriminant):
     a = monomials[2]
     b = monomials[1]
     d = discriminant
     if d > 0:
+        # x1 = -b - √d / 2a
+        # x2 = -b + √d / 2a    
         x1 = fraction(- b - sqrt(d), 2 * a)
         x2 = fraction(- b + sqrt(d), 2 * a)
     elif d == 0:
-        x = fraction(b, 2 * a)
+        # x = -b / 2a    
+        x = fraction(-b, 2 * a)
     else:
+        # x1 = -b - i√-d / 2a
+        # x2 = -b + i√-d / 2a
         x1 = "(%s-i√%s)/%s"%(b*-1, d*-1, 2 * a)
         x2 = "(%s+i√%s)/%s"%(b*-1, d*-1, 2 * a)
     return (x, ) if d == 0 else (x1, x2)
+
+def zero_degree_solution(monomials):
+    a = monomials[0]
+    if a:
+        return ("empty set", )
+    return []
 
 def abs(x):
     return x if x >= 0 else -x
@@ -127,50 +139,55 @@ def sqrt(x):
             return guess
         last_guess= guess
 
-
-def main():
-    print(sys.argv)
-    if len(sys.argv) <> 2:
-        error('Bad number of arguments')
-    equation = sys.argv[1]
-    monomials = list()
-    members = equation.replace(' ', '').split('=')
+def check_syntactic(members):
     if len(members) == 1:
-        error("'=' statement is missing")
+        print_error("'=' statement is missing")
     elif len(members) > 2:
-        error("Too many '=' statements")
+        print_error("Too many '=' statements")
     elif "" in members:
-        error("At least one monomial is empty")
+        print_error("At least one monomial is empty")
+    for member in members:
+        if re.match("^((\+|-|^|^-)((\d+(\.\d+)?)(\*?X(\^(\d+))?)?|((\d+(\.\d+)?)\*?)?X(\^(\d+))?))+$", member, re.IGNORECASE) is None:
+            print_error("parse error")
+
+def find_monomials(members):
+    monomials = []
     for member in members:
         monomial = []
-        # rr = re.search('x(?:\^(\d+))?', member)
         rr = re.finditer('(\+|-|^)(?:(\d+(?:\.\d+)?)\*?)?(x(?:\^(\d+))?)?', member, re.IGNORECASE)
-    
         for item in rr:
             match = item.groups()
-            coef = float(match[0] + match[1] if match[1] else 1)
+            coef = float(match[0] + str(match[1] if match[1] else 1))
             index = (int(match[3]) if match[3] else 1) if match[2] else 0
             if coef <> 0:
                 for n in range(len(monomial), index + 1):
                     monomial.append(0.0)
                 monomial[index] = float(monomial[index]) + coef
         monomials.append(monomial)
+    return merge_monomials(monomials)
 
-    monomials = merge_monomials(monomials)
+
+def main():
+    if len(sys.argv) <> 2:
+        print_error('Bad number of arguments')
+    members = sys.argv[1].replace(' ', '').split('=')
+    check_syntactic(members)
+    monomials = find_monomials(members)
     print_reduce_form(monomials)
     print_polynomial_degree(len(monomials) - 1)
     if len(monomials) >= 4:
-        error("The polynomial degree is stricly greater than 2, I can't solve.")
+            print_error("The polynomial degree is stricly greater than 2, I can't solve.")
     elif len(monomials) == 3:
         d = discriminant(monomials)
         print_discriminant(d)
         print_solutions(second_degree_solution(monomials, d))
     elif len(monomials) == 2:
         print_solutions(first_degree_solution(monomials))
+    elif len(monomials) == 1:
+        print_solutions(zero_degree_solution(monomials))
     else:
-        print("petit malin")
+        print_solutions([])
 
 
 if __name__ == '__main__':
         main()
-
--- test.py
+++ test.py
@@ -16,12 +16,6 @@ os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
 
 main_file = "main.py"
 
-# Le code à tester doit être importable. On
-# verra dans une autre partie comment organiser
-# son projet pour cela.
- 
-# Cette classe est un groupe de tests. Son nom DOIT commencer
-# par 'Test' et la classe DOIT hériter de unittest.TestCase.
 class TestComputerV1(unittest.TestCase):
  
     def cmd_prepare(self):
@@ -88,8 +82,8 @@ class TestComputerV1(unittest.TestCase):
         self.assertAlmostEqual(sqrt(20), 4.472136, places=6)
 
     def test_first_degree_solution(self):
-        self.assertEqual(first_degree_solution([4.0, 2.0]), '-2')
-        self.assertEqual(first_degree_solution([-1.0, 2.0]), '1 / 2')
+        self.assertEqual(first_degree_solution([4.0, 2.0])[0], '-2')
+        self.assertEqual(first_degree_solution([-1.0, 2.0])[0], '1 / 2')
 
     def test_fail_argc(self):
         com = "python %s"%(main_file)
@@ -103,87 +97,154 @@ class TestComputerV1(unittest.TestCase):
      
     def test_success_argc(self):
         com = "hello world"
-        output = self.cmd_output(com)
-        self.assertNotIn("Bad number of arguments", output[0])
+        output = self.cmd_output(com)[0]
+        self.assertNotIn("Bad number of arguments", output)
 
     def test_equal_missing(self):
         com = "Hello world"
-        output = self.cmd_output(com)
-        self.assertIn("'=' statement is missing", output[0])
+        output = self.cmd_output(com)[0]
+        self.assertIn("'=' statement is missing", output)
 
     def test_equal_more(self):
         com = "Hel=lo wo=rld"
-        output = self.cmd_output(com)
-        self.assertIn("Too many '=' statements", output[0])
+        output = self.cmd_output(com)[0]
+        self.assertIn("Too many '=' statements", output)
 
 
     def test_equal_more(self):
         com = "Hello world = "
-        output = self.cmd_output(com)
-        self.assertIn("At least one monomial is empty", output[0])
+        output = self.cmd_output(com)[0]
+        self.assertIn("At least one monomial is empty", output)
 
 
     def test_polynomial_greater(self):
         com = "0 = X^5"
-        output = self.cmd_output(com)
-        self.assertIn("The polynomial degree is stricly greater than 2, I can't solve.", output[0])
-        self.assertIn("Polynomial degree: 5", output[0])
+        output = self.cmd_output(com)[0]
+        self.assertIn("The polynomial degree is stricly greater than 2, I can't solve.", output)
+        self.assertIn("Polynomial degree: 5", output)
 
     def test_calcul_same_monomial(self):
         com = "X^1 + 3 * X^1 - 2 * X^1 = 0"
-        output = self.cmd_output(com)
-        self.assertIn("Polynomial degree: 1", output[0])
-        self.assertIn("Reduced form: 2 * X = 0", output[0])
+        output = self.cmd_output(com)[0]
+        self.assertIn("Polynomial degree: 1", output)
+        self.assertIn("Reduced form: 2 * X = 0", output)
     
     def test_limb_removal_1(self):
         com = "0 = X^1 + 3 * X^2 - 2"
-        output = self.cmd_output(com)
-        self.assertIn("Reduced form: - 3 * X^2 - X + 2 = 0", output[0])
+        output = self.cmd_output(com)[0]
+        self.assertIn("Reduced form: - 3 * X^2 - X + 2 = 0", output)
 
 
     def test_limb_removal_2(self):
         com = "X^1 -4 * X^2 + X^0 = X^1 + 3 * X^2 - 2"
-        output = self.cmd_output(com)
-        self.assertIn("Reduced form: - 7 * X^2 + 3 = 0", output[0])
+        output = self.cmd_output(com)[0]
+        self.assertIn("Reduced form: - 7 * X^2 + 3 = 0", output)
 
 
-# Ceci lance le test si on exécute le script
-# directement.
+    def test_solution_linear_function(self):
+        com = "2 * X^1 + 4 = 0"
+        output = self.cmd_output(com)[0]
+        self.assertIn("Polynomial degree: 1", output)
+        self.assertIn("x: -2", output)
+        com = "2 * X^1 - 8 * X^0 = 0"
+        output = self.cmd_output(com)[0]
+        self.assertIn("Polynomial degree: 1", output)
+        self.assertIn("x: 4", output)
+
+    def test_two_solutions_polynomial_function(self):
+        com = "- 1 * X^2 + 2 * X^1 + 3 = 0"
+        output = self.cmd_output(com)[0]
+        self.assertIn("Polynomial degree: 2", output)
+        self.assertIn("x1: 3", output)
+        self.assertIn("x2: -1", output)
+
+    def test_one_solution_polynomial_function(self):
+        com = "1 * X^2 + 2 * X^1 + 1 = 0"
+        output = self.cmd_output(com)[0]
+        self.assertIn("Polynomial degree: 2", output)
+        self.assertIn("x: -1", output)
+
+    def test_complex_solution_polynomial_function(self):
+        com = "4 * X^2 + 2 * X^1 + 1 = 0 "
+        output = self.cmd_output(com)[0]
+        self.assertIn("Polynomial degree: 2", output)
+        self.assertIn("x1: (-2−i√12) / 8", output)
+        self.assertIn("x2: (-2+i√12) / 8", output)
+
+    def test_real_solution_polynomial_function(self):
+        com = "4 * X^2 - 4 * X^2 = 0 "
+        output = self.cmd_output(com)[0]
+        self.assertIn("x is a real", output)
+
+    def test_real_solution_polynomial_function(self):
+        com = "4 * X^0 = 0 "
+        output = self.cmd_output(com)[0]
+        self.assertIn("x: empty set", output)
+
+    def test_with_float_monomials(self):
+        com = "1.05 * X^2 + 2.5 * X^1 + 0.2 = 0"
+        output = self.cmd_output(com)[0]
+        self.assertIn("Polynomial degree: 2", output)
+        self.assertIn("x1: -2.29806", output)
+        self.assertIn("x2: -0.08288", output)
+
+    def test_with_zero_monomials(self):
+        com = "1 * X^2 + 0 * X^1 - 1 = 0"
+        output = self.cmd_output(com)[0]
+        self.assertIn("Polynomial degree: 2", output)
+        self.assertIn("Reduced form: X^2 - 1 = 0", output)
+        self.assertIn("x1: -1", output)
+        self.assertIn("x2: 1", output)
+
+    def test_with_zero_monomials_greater_polynomial(self):
+        com = "-X^2 + 2 * X^1 + 3 = 0 * X^5"
+        output = self.cmd_output(com)[0]
+        self.assertIn("Polynomial degree: 2", output)
+        self.assertIn("Reduced form: - X^2 + 2 * X + 3 = 0", output)
+        self.assertIn("x1: 3", output)
+        self.assertIn("x2: -1", output)
+
+    def test_fraction_in_solution(self):
+        com = "10 * X^2 + 2 * X^1 = 0 "
+        output = self.cmd_output(com)[0]
+        self.assertIn("x1: -1 / 5", output)
+        self.assertIn("x2: 0", output)
+
+    def test_with_operator_multiplicate(self):
+        com = "4X^2 + 4X^1 + 4 = 0"
+        output = self.cmd_output(com)[0]
+        self.assertIn("Reduced form: 4 * X^2 + 4 * X + 4 = 0", output)
+
+    def test_with_operator_power(self):
+        com = "4 * X^2 + 4 * X + 4 = 0"
+        output = self.cmd_output(com)[0]
+        self.assertIn("Reduced form: 4 * X^2 + 4 * X + 4 = 0", output)
+
+    def test_with_operator_power(self):
+        com = "4 * X^2 - 4 * X^1 = -4 * X^0 - 8 * X^1"
+        output = self.cmd_output(com)[0]
+        self.assertIn("Reduced form: 4 * X^2 + 4 * X + 4 = 0", output)
+
+
 if __name__ == '__main__':
     unittest.main()
-
-
-
-# # Chaque méthode dont le nom commence par 'test_'
-# # est un test.
-# def test_missing_argv(self):
-#     output = cmd_output("")[0]
-#     self.assertEqual(output, 'Argument is missing')
-
-
-# print("##### Syntactic tests #####")
-# print("##### argument missing")
-# print("##### too many arguments")
-# print("##### '=' missing")
-# print("##### too many '='")
-# print("##### monomial empty")
-# print("##### different unknown of x")
-# print("##### lexical 1")
-# print("##### lexical 2")
-# print("##### lexical 3")
 
 # print("##### Function Tests #####")
 # print("##### polynomial greater than 2") # x^5 = 0
 # print("##### calcul same monomial") # x^1 + 3 * x^1 - 2 * x^1 = 0 => reduce form   2 * x^1 = 0
 # print("##### limb removal 1 ") # 0 = x^1 + 3 * x^2 - 2 = 0 => reduce form -3 * x^2 - x^1 + 2 = 0
 # print("##### limb removal 2 ") # x^1 -4 * x^2 + x^0 = x^1 + 3 * x^2 - 2 => reduce form -7 * x^2 + 3 = 0
-# print("##### solution test 1 linear function")  # 2 * x^1 + 4 = 0 => x = -2
-# print("##### solution test 2 linear function")  # 2 * x^1 - 8 * x^0 = 0 => x = 4
+# print("##### solution 1 linear function")  # 2 * x^1 + 4 = 0 => x = -2
+# print("##### solution 2 linear function")  # 2 * x^1 - 8 * x^0 = 0 => x = 4
 # print("##### two solutions")  # -x^2 + 2 * x^1 + 3 = 0 => x1 = 3 et x2 = -1
 # print("##### one solution")  # 1 * x^2 + 2 * x^1 + 1 = 0 => -1
 # print("##### complex solution")  # 4 * x^2 + 2 * x^1 + 1 = 0 => x1 = (-2−i√12) / 8 et x2 = (-2+i√12) / 8
-# print("##### real solution")  # 4 * x^2 - 4 * x^2  = 0 => real
+# print("##### real solution")  # 4 * x^2 - 4 * x^2  = 0 => x: ℝ
 # print("##### no solution")  # 4 * x^0  = 0 => empty set
+# print("##### with float")  # 1.05 * x^2 + 2.5 * x^1 + 0.2 = 0 => x1 = -2.2980669856774 et x2 = -0.082885395274952
+# print("##### with zeros")  # 1 * x^2 + 0 * x^1 - 1 = 0 => x = 1
+# print("##### with zeros in greater polinomial")  # -x^2 + 2 * x^1 + 3 = 0 * X^5 => x1 = 3 et x2 = -1
+
 
 # print("##### BONUS Tests #####")
 # print("##### fraction") # 4 * x^2 + 4 * x^1 + 4 * x^0 = 0 => x = -1/2
